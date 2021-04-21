d5780591a1e0afff488d6d5bedbae2bb4ded2cd1
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-21 10:46:09 +0900

    イベントハンドラー

--- src/components/Basic1.js
+++ src/components/Basic1.js
@@ -4,12 +4,21 @@ import React from 'react'
 // Functionコンポーネンtの(reactではClassコンポーネントは基本使わない)
 // 親からのデータを受け取る際は引数へpropsを記載
 const Basic1 = (props) => {
+
+  // イベント関数
+  const click = () => {
+    alert("click");
+  }
+
   return (
     <div>
 
       {/* 引数で受け取ったpropsの変数を指定して表示する */}
       <h1>Hello React1 {props.name}  </h1>
       <h1>Hello React2</h1>
+
+      {/* イベントハンドラー */}
+      <button onClick={click}>click</button>
     </div>
   )
 }
