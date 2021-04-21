ca22c15c4183c1986443e3b43afcf1ab512eba1a
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-21 10:37:34 +0900

    こんぽーねんと

---
+++ src/components/Basic1.js
@@ -0,0 +1,14 @@
+// raで補完メニューが出てくるrafceを選択
+import React from 'react'
+
+// Functionコンポーネンtの(reactではClassコンポーネントは基本使わない)
+const Basic1 = () => {
+  return (
+    <div>
+      <h1>Hello React1</h1>
+      <h1>Hello React2</h1>
+    </div>
+  )
+}
+
+export default Basic1
