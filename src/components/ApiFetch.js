0d1aecb81c5c4db7b2cb07cb0bceec1b36d09217
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-21 21:42:10 +0900

    APIから特定のデータ取得

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -5,6 +5,13 @@ import axios from 'axios'
 const ApiFetch = () => {
 
   const [posts, setPosts] = useState([])
+  // IDを管理するState
+  const [id, setId] = useState(1)
+  // ID入力判定
+  const [clicked, setClicked] = useState(false)
+  const handlerClick=() => {
+    setClicked(!clicked)
+  }
   useEffect(() => {
     // // axiosを使用して外部APIを使う
     // axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -16,22 +23,31 @@ const ApiFetch = () => {
 
 
     // fetchの場合
-    fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'})
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{method: 'GET'})
     // fetchではHTMLの形で返されるので、jsonの形に直すため、.jsonを記載している
     .then(res => res.json())
     .then(data => {
       setPosts(data)
     })
     // 最初の一回のみ取得すれば良いので[]を記載する
-  },[])
+    // 変化した値を見て変更させるならば第二引数へ変数を指定する必要がある
+  },[clicked])
 
   return (
     <div>
-      <ul>
+      {/* <ul>
         {
           posts.map(post => <li key={post.id}>{post.id}: {post.title} </li>)
         }
-      </ul>
+      </ul> */}
+      <input type="text"
+       name=""
+        id=""
+        value={id}
+        onChange={e=>setId(e.target.value)}
+        />
+        <button onClick={handlerClick}>検索</button>
+        <p>{posts.title}</p>
     </div>
   )
 }
