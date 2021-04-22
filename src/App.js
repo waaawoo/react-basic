7491a4320a3f054bed6811d2c79ed3465e5276bc
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-22 10:17:54 +0900

    useContext

--- src/App.js
+++ src/App.js
@@ -7,24 +7,32 @@ import Basic2 from "./components/Basic2"
 import Basic3 from "./components/BasicUseEffect"
 import TimerCom from "./components/TimerContainer"
 import ApiFetch from "./components/ApiFetch"
+import AppContext from "./components/contexts/AppContext"
+import B from "./components/contexts/B"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
 
-        {/* コンポーネント呼び出し name= 子コンポーネントへデータを渡す */}
-        <Basic1 name="Hooks" />
-        <hr/>
-        <Basic2 />
-        <hr/>
-        <Basic3/>
-        <hr/>
-        <TimerCom/>
-        <hr/>
-        <ApiFetch/>
-      </header>
-    </div>
+    // Contextのproviderで囲ったコンポーネントないで使用できる
+    <AppContext.Provider value={"value from App.js"}>
+      <div className="App">
+        <header className="App-header">
+
+          {/* コンポーネント呼び出し name= 子コンポーネントへデータを渡す */}
+          <Basic1 name="Hooks" />
+          <hr/>
+          <Basic2 />
+          <hr/>
+          <Basic3/>
+          <hr/>
+          <TimerCom/>
+          <hr/>
+          <ApiFetch/>
+          <hr/>
+          <B />
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/components/BasicReducer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BasicReducer = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default BasicReducer
